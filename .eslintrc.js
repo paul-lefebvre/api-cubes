e5ed8ce97e383adbc5e6c12a30874ae898c7c011
Paul LEFEBVRE <lefebvre@antadis.com>
2022-01-12 02:33:23 +0100

    hybride Vue with express on same server

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     node: true,
   },
   extends: [
+    "plugin:vue/vue3-essential",
     'eslint:recommended',
   ],
   parserOptions: {
--- app.js
+++ app.js
@@ -1,50 +1,70 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'src/views'));
-app.set('view engine', 'pug');
-
+// SETUP APP
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/dist', express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
-// Swagger UI & Options
+
+// OPTIONS
+const vueOptions = {
+    root: 'dist',
+    dotfiles: 'deny',
+    headers: {
+        'dina-timestamp': Date.now(),
+        'my-xxx-header': true
+    }
+};
+
 var swaggerOptions = {
   customSiteTitle: "CUBES",
   customfavIcon: "/public/favicon.ico",
   customCss: '.swagger-ui .topbar { display: none }',
 };
 
+// ROUTES API
+app.get('/api', (req, res) => {
+  res.json({message: 'Welcome to the Server'});
+});
+
+// USE INDEX.HTML OF PUBLIC FOLDER TO USE VUE BUNDLE
+app.get('/', (req, resp) => {
+  resp.sendFile('index.html', vueOptions, (err) => {
+        if (!err)
+            console.log('Welcome on BACK OFFICE');
+        else
+            console.log('Failed to serve VUE');
+    })
+});
+
+
+// SWAGGER UI FOR DOC API
 app.use(
   '/doc',
   swaggerUi.serve, 
   swaggerUi.setup(swaggerDocument, swaggerOptions)
 );
 
-// catch 404 and forward to error handler
+// CATCH 404 ERRORS
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+// OTHERS ERRORS HANDLER
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
