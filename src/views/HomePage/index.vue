94b8cde4bc90ad9f932ec5a82448b99ed985e42b
Paul LEFEBVRE <lefebvre@antadis.com>
2022-01-25 14:13:37 +0100

    add test fetch api from backoffice

--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -1,19 +1,59 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome test Your Vue.js App"/>
+    <HelloWorld msg="Welcome test Your Vue.js App" />
+    <h3>Résultat d'un CALL API (vers /api/users/)</h3>
+    <p v-bind:key="user.id" v-for="user in users">
+      Utilisateur : {{ user.firstname }} {{ user.lastname }} | ID : {{ user.usr_id }}
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+
+async function getUsers() {
+  try {
+    let users = await fetch('/api/users/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(res => res.json())
+    .then((response) => {
+      return response;
+    });
+
+    console.log('RESPONSE : ', users);
+    return users;
+
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 export default {
   name: 'HomePage',
+  props: {},
   components: {
     HelloWorld
-  }
+  },
+
+  data () {
+    return  {
+      users: []
+    }
+  },
+  async mounted () {
+    console.log('users : ', this.users);
+    console.log('-- CALL API --');
+    this.users = await getUsers();
+    console.log('users : ', this.users);
+  },
+
 }
 </script>
 
