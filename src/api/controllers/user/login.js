6d5ba58b1e10306bc88488d910e0dc2723cd5586
Béatrice <beatrice.briand@viacesi.fr>
2022-03-24 15:46:55 +0100

    modif login

--- src/api/controllers/user/login.js
+++ src/api/controllers/user/login.js
@@ -8,12 +8,21 @@ export default async function (req, res) {
   const { mail, password } = req.body;
 
   try {
-    const userWithMail = await User.findOne({ where: { [Op.or]: [{ mail }] } });
+    const userWithMail = await User.findOne({
+      where: { [Op.or]: [{ mail }] },
+      include: [
+        'abonnements',
+        'abonnes',
+        'publications',
+      ]
+    });
+    
+    
     if (!userWithMail) {
-      return res.status(500).send({ message: "ce mail est inconnu" });
+      return res.status(500).send({ message: "Mail ou mot de passe invalide", error: true });
     }
     if (userWithMail.password !== password) {
-      return res.status(501).send({ message: "mot de passe incorrect" });
+      return res.status(501).send({ message: "Mail ou mot de passe invalide", error: true });
     }
 
     const token = jwt.sign(
@@ -26,14 +35,16 @@ export default async function (req, res) {
       .status(201)
       .json({ user: userWithMail, acces_token: token })
       .send({
-        message: "connection réussie!",
+        message: "Connection réussie!",
         user: userWithMail,
         acces_token: token,
+        error: false,
       });
   } catch (e) {
     console.log(e);
     return res.status(404).send({
-      message: "requête impossible" + e.message,
+      message: "Requête impossible" + e.message,
+      error: true,
     });
   }
 }
